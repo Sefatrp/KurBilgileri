196df843bbfb3c59017634583f940bc55f016a1f
Sefatrp <Sefatrp@gmail.com>
2015-09-02 16:25:26 +0300

    Kur Bilgisi Alma

---
+++ KurBilgileri/Form1.Designer.cs
@@ -0,0 +1,206 @@
+﻿namespace KurBilgileri
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lbDoviz = new System.Windows.Forms.ListBox();
+            this.btnGetir = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.lblBirim = new System.Windows.Forms.Label();
+            this.lblAlis = new System.Windows.Forms.Label();
+            this.lblSatis = new System.Windows.Forms.Label();
+            this.maskedTextBox1 = new System.Windows.Forms.MaskedTextBox();
+            this.dateTimePicker1 = new System.Windows.Forms.DateTimePicker();
+            this.btnHesap = new System.Windows.Forms.Button();
+            this.txtHesap = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // lbDoviz
+            // 
+            this.lbDoviz.Font = new System.Drawing.Font("Microsoft Sans Serif", 11F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.lbDoviz.FormattingEnabled = true;
+            this.lbDoviz.ItemHeight = 18;
+            this.lbDoviz.Location = new System.Drawing.Point(12, 68);
+            this.lbDoviz.Name = "lbDoviz";
+            this.lbDoviz.Size = new System.Drawing.Size(164, 328);
+            this.lbDoviz.TabIndex = 0;
+            this.lbDoviz.SelectedIndexChanged += new System.EventHandler(this.lbDoviz_SelectedIndexChanged);
+            // 
+            // btnGetir
+            // 
+            this.btnGetir.Font = new System.Drawing.Font("Microsoft Sans Serif", 11F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.btnGetir.Location = new System.Drawing.Point(313, 12);
+            this.btnGetir.Name = "btnGetir";
+            this.btnGetir.Size = new System.Drawing.Size(107, 53);
+            this.btnGetir.TabIndex = 1;
+            this.btnGetir.Text = "Kur Bilgisi Getir";
+            this.btnGetir.UseVisualStyleBackColor = true;
+            this.btnGetir.Click += new System.EventHandler(this.btnGetir_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label1.Location = new System.Drawing.Point(223, 131);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(48, 20);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Birim:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label2.Location = new System.Drawing.Point(223, 167);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(79, 20);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Alış Fiyatı:";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label3.Location = new System.Drawing.Point(223, 205);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(90, 20);
+            this.label3.TabIndex = 4;
+            this.label3.Text = "Satış Fiyatı:";
+            // 
+            // lblBirim
+            // 
+            this.lblBirim.AutoSize = true;
+            this.lblBirim.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.lblBirim.Location = new System.Drawing.Point(349, 131);
+            this.lblBirim.Name = "lblBirim";
+            this.lblBirim.Size = new System.Drawing.Size(18, 20);
+            this.lblBirim.TabIndex = 5;
+            this.lblBirim.Text = "0";
+            // 
+            // lblAlis
+            // 
+            this.lblAlis.AutoSize = true;
+            this.lblAlis.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.lblAlis.Location = new System.Drawing.Point(349, 167);
+            this.lblAlis.Name = "lblAlis";
+            this.lblAlis.Size = new System.Drawing.Size(18, 20);
+            this.lblAlis.TabIndex = 6;
+            this.lblAlis.Text = "0";
+            // 
+            // lblSatis
+            // 
+            this.lblSatis.AutoSize = true;
+            this.lblSatis.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.lblSatis.Location = new System.Drawing.Point(349, 205);
+            this.lblSatis.Name = "lblSatis";
+            this.lblSatis.Size = new System.Drawing.Size(18, 20);
+            this.lblSatis.TabIndex = 7;
+            this.lblSatis.Text = "0";
+            // 
+            // maskedTextBox1
+            // 
+            this.maskedTextBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 11F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.maskedTextBox1.Location = new System.Drawing.Point(227, 263);
+            this.maskedTextBox1.Mask = "0000000";
+            this.maskedTextBox1.Name = "maskedTextBox1";
+            this.maskedTextBox1.RightToLeft = System.Windows.Forms.RightToLeft.Yes;
+            this.maskedTextBox1.Size = new System.Drawing.Size(140, 24);
+            this.maskedTextBox1.TabIndex = 8;
+            // 
+            // dateTimePicker1
+            // 
+            this.dateTimePicker1.Font = new System.Drawing.Font("Microsoft Sans Serif", 11F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.dateTimePicker1.Location = new System.Drawing.Point(12, 12);
+            this.dateTimePicker1.Name = "dateTimePicker1";
+            this.dateTimePicker1.Size = new System.Drawing.Size(200, 24);
+            this.dateTimePicker1.TabIndex = 9;
+            // 
+            // btnHesap
+            // 
+            this.btnHesap.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.btnHesap.Location = new System.Drawing.Point(292, 323);
+            this.btnHesap.Name = "btnHesap";
+            this.btnHesap.Size = new System.Drawing.Size(75, 29);
+            this.btnHesap.TabIndex = 10;
+            this.btnHesap.Text = "Hesapla";
+            this.btnHesap.UseVisualStyleBackColor = true;
+            this.btnHesap.Click += new System.EventHandler(this.btnHesap_Click);
+            // 
+            // txtHesap
+            // 
+            this.txtHesap.Font = new System.Drawing.Font("Microsoft Sans Serif", 11F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.txtHesap.Location = new System.Drawing.Point(227, 376);
+            this.txtHesap.Name = "txtHesap";
+            this.txtHesap.ReadOnly = true;
+            this.txtHesap.Size = new System.Drawing.Size(140, 24);
+            this.txtHesap.TabIndex = 11;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(432, 444);
+            this.Controls.Add(this.txtHesap);
+            this.Controls.Add(this.btnHesap);
+            this.Controls.Add(this.dateTimePicker1);
+            this.Controls.Add(this.maskedTextBox1);
+            this.Controls.Add(this.lblSatis);
+            this.Controls.Add(this.lblAlis);
+            this.Controls.Add(this.lblBirim);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.btnGetir);
+            this.Controls.Add(this.lbDoviz);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ListBox lbDoviz;
+        private System.Windows.Forms.Button btnGetir;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label lblBirim;
+        private System.Windows.Forms.Label lblAlis;
+        private System.Windows.Forms.Label lblSatis;
+        private System.Windows.Forms.MaskedTextBox maskedTextBox1;
+        private System.Windows.Forms.DateTimePicker dateTimePicker1;
+        private System.Windows.Forms.Button btnHesap;
+        private System.Windows.Forms.TextBox txtHesap;
+    }
+}
+
---
+++ KurBilgileri/Form1.cs
@@ -0,0 +1,105 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Xml;
+using System.IO;
+
+
+namespace KurBilgileri
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        DataTable dt = new DataTable();
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            dt.Columns.Add("Birim");
+            dt.Columns.Add("Döviz Adı");
+            dt.Columns.Add("Alış Fiyatı");
+            dt.Columns.Add("Satış Fiyatı");
+            
+        }
+
+        private void btnGetir_Click(object sender, EventArgs e)
+        {
+            if (dateTimePicker1.Value>DateTime.Now)
+            {
+                //gelecek tarih için dosya zaten olmayacak
+            }
+            dt.Clear();
+            string xmlYolu = "";
+            //bugün için http://www.tcmb.gov.tr/kurlar/today.xml
+            //geçmiş için http://www.tcmb.gov.tr/kurlar/201502/12022015.xml
+
+            if (dateTimePicker1.Value.ToShortDateString()==DateTime.Now.ToShortDateString())//bugün için kur bilgisi gelsin
+            {
+                xmlYolu = "http://www.tcmb.gov.tr/kurlar/today.xml";
+            }
+            else
+            {
+                xmlYolu = "http://www.tcmb.gov.tr/kurlar/" + dateTimePicker1.Value.Year.ToString("0000") + dateTimePicker1.Value.Month.ToString("00") + "/" +
+                    dateTimePicker1.Value.Day.ToString("00") +
+                    dateTimePicker1.Value.Month.ToString("00") +
+                    dateTimePicker1.Value.Year.ToString("0000") + ".xml";
+            }
+            XmlTextReader okuyucu = new XmlTextReader(xmlYolu);
+            XmlDocument dokuman = new XmlDocument();
+            try
+            {
+                dokuman.Load(okuyucu);
+            }
+            catch 
+            {
+                MessageBox.Show("bu tarihe ait bir kur bilgisi yok");
+            }
+            XmlElement rootEleman = dokuman.DocumentElement;
+            XmlNodeList currencyList = dokuman.GetElementsByTagName("Currency");
+            foreach (XmlNode item in currencyList)
+            {
+                if (item["Unit"]==null || item["Isim"]==null || item["ForexBuying"]==null || item["ForexSelling"]==null)
+                {
+                    continue;//eğer eksik bilgi varsa döviz bilgisini almayıver.
+                }
+                DataRow satir = dt.NewRow();
+                satir[0] = item["Unit"].InnerText;
+                satir[1] = item["Isim"].InnerText;
+                satir[2] = item["ForexBuying"].InnerText;
+                satir[3] = item["ForexSelling"].InnerText;
+
+                dt.Rows.Add(satir);
+            }
+            lbDoviz.DataSource = dt;
+            lbDoviz.DisplayMember = "Döviz Adı";
+        }
+
+        private void lbDoviz_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            if (lbDoviz.SelectedIndex>-1)
+            {
+                int index=lbDoviz.SelectedIndex;
+                lblBirim.Text = dt.Rows[index][0].ToString();
+                lblAlis.Text = dt.Rows[index][2].ToString();
+                lblSatis.Text = dt.Rows[index][3].ToString();
+            }
+        }
+
+        private void btnHesap_Click(object sender, EventArgs e)
+        {
+            int miktar;
+            double neKadar = double.Parse(lblAlis.Text.Replace(".",","));
+            if (int.TryParse(maskedTextBox1.Text, out miktar))
+            {
+                txtHesap.Text = (miktar * neKadar).ToString();
+            }
+        }
+    }
+}
